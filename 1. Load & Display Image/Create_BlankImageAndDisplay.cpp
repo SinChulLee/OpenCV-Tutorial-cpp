957ac91f59801ceecf5d5a0c3d42e0882452818c
이신철 <145883892+SinChulLee@users.noreply.github.com>
2024-05-22 17:46:41 +0900

    Update Create_BlankImageAndDisplay.cpp
    한글 설명 편집

--- 1. Load & Display Image/Create_BlankImageAndDisplay.cpp
+++ 1. Load & Display Image/Create_BlankImageAndDisplay.cpp
@@ -7,25 +7,24 @@
 using namespace cv;
 using namespace std;
 
-// Create a Blank Image & Display
+// 빈 이미지 생성 및 표시
 int main(int argc, char **argv)
 {
-    // create a new image which consists of
-    // 3 channels
-    // image depth of 8 bits
-    // 800 x 600 of resolution (800 wide and 600 high)
-    // each pixels initialized to the value of (100, 250, 30) for Blue, Green and Red planes respectively.
+    // 3개의 채널로 구성된 새로운 이미지를 생성
+    // 이미지 depth -> 8bit
+    // 800 x 600 해상도 (넓이 800, 높이 600)
+    // 각 픽셀은 Blue, Green, Red에 대해 각각 (100, 250, 30) 값으로 초기화.
     Mat image(600, 800, CV_8UC3, Scalar(100, 250, 30));
 
-    String windowName = "Window with Blank Image"; // Name of the window
+    String windowName = "Window with Blank Image"; // window의 이름
 
-    namedWindow(windowName); // Create a window
+    namedWindow(windowName); // window 생성
 
-    imshow(windowName, image); // Show our image inside the created window.
+    imshow(windowName, image); // 생성된 window에 image 표시
 
-    waitKey(0); // Wait for any keystroke in the window
+    waitKey(0); // window에서 키 입력까지 wait
 
-    destroyWindow(windowName); // destroy the created window
+    destroyWindow(windowName); // 생성된 window destroy
 
     return 0;
 }
